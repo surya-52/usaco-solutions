108335d0a1c1bc6f04d0a648954da622a345488d
surya-52 <107560969+surya-52@users.noreply.github.com>
2023-10-22 00:06:57 +0530

    Create block_game.cpp

---
+++ block_game.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#include <numeric>
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("blocks.in", "r", stdin);
+    freopen("blocks.out", "w", stdout);
+#endif
+}
+
+
+signed main()
+{   
+    c_p_c();
+    int t = 1;
+    // cin >> t;
+    while(t--){
+      int n;
+      cin >> n;
+      vector<int> alpha(26,0);
+      for(int i = 0 ; i < n ; i++){
+        string s1,s2;
+        cin >> s1 >> s2;
+        vector<int> temp(26,0);
+        for(int i = 0 ; i < s1.length() ; i++){
+            temp[s1[i]-'a']++;
+        }
+        vector<int> temp2(26,0);
+        for(int i = 0 ; i < s2.length() ; i++){
+            temp2[s2[i]-'a']++;
+        }
+        for(int i = 0 ; i < 26 ; i++){
+            alpha[i] += max(temp[i],temp2[i]);
+        }
+      }
+      for(auto x:alpha){
+        cout<<x<<endl;
+      }
+
+      }
+      
+    return 0;
+      }
