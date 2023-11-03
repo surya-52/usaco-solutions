b6d2d074a7d5f16400181059441b716e895110b1
surya-52 <107560969+surya-52@users.noreply.github.com>
2023-11-03 20:02:27 +0530

    Create counting_liars.cpp

---
+++ counting_liars.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#include <numeric>
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("diamond.in", "r", stdin);
+    freopen("diamond.out", "w", stdout);
+#endif
+}
+
+
+int32_t main()
+{   
+    // c_p_c();
+    int t = 1;
+    // cin >> t;
+    while(t--){
+    int n;
+    cin >> n;
+    vector<pair<int,int>> v(n);
+    for(int i = 0 ; i < n ; i++){
+        char c;
+        cin >> c;
+        cin >> v[i].first;
+        if(c == 'G'){
+            v[i].second = 1;
+        }
+        else {
+            v[i].second = 0;
+        }
+    }
+    sort(v.begin(),v.end());
+    int ans = INT_MAX;
+    for(int i = 0 ; i < n ; i++){
+       int ct = 0;
+       for(int j = 0 ; j < i ; j++){
+        if(v[j].second==0) ct++;
+       }
+       for(int j = i+1 ; j < n ; j++){
+        if(v[j].second==1) ct++;
+       }
+       ans = min(ans,ct);
+    }
+   cout<<ans<<endl;
+
+    }
+    return 0;
+      }
